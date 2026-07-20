0a487abc698ca302e06d88ab24542b52aaf532de
Irasov <srnan@yandex.ru>
2026-07-20 14:31:42 +0500

    feat: add active class for click(accordion)

--- accordion/scripts/main.js
+++ accordion/scripts/main.js
@@ -1,7 +1,18 @@
 const initOptions = () => {
   const optionsContainer = document.querySelector('.options');
   const options = document.querySelectorAll('.option');
+
   optionsContainer.style.setProperty('--total-options', options.length);
+  optionsContainer.addEventListener('click', (e) => {
+    const clickedOption = e.target.closest('.option');
+    if (!clickedOption || clickedOption.classList.contains('active')) {
+      return;
+    }
+    options.forEach((option) => {
+      option.classList.remove('active');
+    });
+    clickedOption.classList.add('active');
+  });
 };
 
 document.addEventListener('DOMContentLoaded', initOptions);
